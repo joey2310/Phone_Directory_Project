9153781850cd27e580e733f13b999a0718476cc0
joey2310 <bhagatpratik23@gmail.com>
2023-07-29 20:28:20 +0530

    Added 'Delete' button

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ class App extends Component {
         return <div key={sub.id} className="grid-container">
           <span className="gird-item">{sub.name}</span>
           <span className="gird-item">{sub.phone}</span>
+          <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
         </div>  
       })
      }
