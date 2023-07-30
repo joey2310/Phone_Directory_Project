e1960eefe2c5d35914b3d1c96bd241519a8d9e7a
joey2310 <bhagatpratik23@gmail.com>
2023-07-30 23:16:20 +0530

    Added 'AddSubscriber' component

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,12 @@
-import React,{Component} from 'react';
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
-const Header = function(props){
-     return(
-         <div className="header">
-             {props.heading}
-            </div>
-        )
-    }
-
+const Header = function (props) {
+    return (
+        <div className="header">
+            {props.heading}
+        </div>
+    )
+}
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import AddSubscriber from './AddSubcriber';
-import reportWebVitals from './reportWebVitals';
-import './common/common.css'
+import registerServiceWorker from './registerServiceWorker';
+import './common/common.css';
+import PhoneDirectory from './PhoneDirectory';
 
-ReactDOM.render(<AddSubscriber/>, document.getElementById('root'));
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
 registerServiceWorker();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
